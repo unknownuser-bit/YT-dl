11c79698e4a43724907aa03470e2bbe0b8b7698b
NimaVahedi <>
2026-05-12 08:58:42 +0330

    refactor(scripts): more logging

--- scripts/block_untill_local_warp_is_active.sh
+++ scripts/block_untill_local_warp_is_active.sh
@@ -4,9 +4,12 @@ if [ -z $tries ]; then
     tries=30
 fi
 
+echo "waiting for warp $tries times"
+
 for i in {1..$tries}; do
     sleep 1
     RESULT=$(curl -s --max-time 10 --socks5-hostname 127.0.0.1:1080 https://cloudflare.com/cdn-cgi/trace 2>/dev/null || echo "failed")
+    echo "$RESULT"
     if echo "$RESULT" | grep -qE "warp=(on|plus)"; then
         echo "✅ WARP is active!"
         echo "$RESULT"
@@ -14,4 +17,5 @@ for i in {1..$tries}; do
     fi
 done
 
+echo "warp was not activated"
 exit 1
